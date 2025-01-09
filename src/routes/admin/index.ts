3f50ce87ec57ce3d4061eeb6a103b1a9376909d2
MaivMinh <maivanminh.se@gmail.com>
2025-01-09 21:41:12 +0700

    Init structure

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,6 +1,36 @@
+import { HomeOutlined } from '@ant-design/icons';
+import ManageAccounts from 'containers/admin/ManageAccounts.tsx';
 import { IRoute } from 'interfaces';
 
 const adminRoutes: IRoute[] = [
-  // TODO: add route for admin
+  // TODO: add more routes for admin
+
+  {
+    exact: true,
+    path: '/',
+    name: 'Tài khoản',
+    component: ManageAccounts,
+    icon: HomeOutlined,
+    children: ['/accounts', '/players', '/brands'],
+  },
+  {
+    exact: true,
+    path: '/accounts',
+    name: 'Quản lý tài khoản',
+    children: [],
+  },
+  {
+    exact: true,
+    path: '/players',
+    name: 'Quản lý người chơi',
+    children: [],
+  },
+  {
+    exact: true,
+    path: '/brands',
+    name: 'Quản lý thương hiệu',
+    children: [],
+  },
 ];
+
 export default adminRoutes;
